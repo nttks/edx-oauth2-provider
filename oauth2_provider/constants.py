95e8b90c9379be68c5281a0eed7d20fe1204cd71
Nimisha Asthagiri <nasthagiri@edx.org>
2015-02-23 22:48:51 -0500

    Support READ_SCOPE and WRITE_SCOPE.

--- oauth2_provider/constants.py
+++ oauth2_provider/constants.py
@@ -22,6 +22,8 @@ EMAIL_SCOPE = 1 << 2
 COURSE_STAFF_SCOPE = 1 << 3
 COURSE_INSTRUCTOR_SCOPE = 1 << 4
 PERMISSIONS = 1 << 5
+READ_SCOPE = 1 << 6
+WRITE_SCOPE = 1 << 7
 
 # Scope setting as required by django-oauth2-provider
 # The default scope value is SCOPES[0][0], which in this case is zero.
@@ -34,7 +36,9 @@ SCOPES = (
     (EMAIL_SCOPE, 'email'),
     (COURSE_STAFF_SCOPE, 'course_staff'),
     (COURSE_INSTRUCTOR_SCOPE, 'course_instructor'),
-    (PERMISSIONS, 'permissions')
+    (PERMISSIONS, 'permissions'),
+    (READ_SCOPE, 'read'),
+    (WRITE_SCOPE, 'write'),
 )
 
 SCOPE_NAMES = [(name, name) for (value, name) in SCOPES]
